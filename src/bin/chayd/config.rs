8928caecac45d7a79d8726c466848eba4ec2e72e
Kevin Greene <kgreenek@gmail.com>
2023-03-22 16:25:31 -0700

    Add support for environment variables in config

--- src/bin/chayd/config.rs
+++ src/bin/chayd/config.rs
@@ -95,12 +95,15 @@ impl RenderedProgramConfig {
         program_config: &ProgramConfig,
     ) -> Result<Self, Box<dyn std::error::Error>> {
         let mut rendered_config = Self::default();
-        let mut vars_renderer = VarsRenderer::new(&config.vars);
+        let mut vars_renderer = VarsRenderer::new(&config.vars)?;
         rendered_config.program = Self::render_program(program_config, &mut vars_renderer)?;
         if let Some(logger_name) = &program_config.logger {
             if let Some(logger_config) = config.loggers.get(logger_name) {
-                rendered_config.logger =
-                    Some(Self::render_logger(logger_config, program_name, &mut vars_renderer)?);
+                rendered_config.logger = Some(Self::render_logger(
+                    logger_config,
+                    program_name,
+                    &mut vars_renderer,
+                )?);
             } else {
                 return Err(format!("Logger not found: {logger_name}").into());
             }
@@ -113,12 +116,11 @@ impl RenderedProgramConfig {
         vars_renderer: &mut VarsRenderer,
     ) -> Result<ProgramConfig, tera::Error> {
         let mut rendered_program_config = program_config.clone();
-        rendered_program_config.command =
-            vars_renderer.render_str(program_config.command.clone())?;
+        rendered_program_config.command = vars_renderer.render_str(&program_config.command)?;
         if let Some(args) = &program_config.args {
             let mut rendered_args: Vec<String> = vec![];
             for arg in args {
-                let rendered_arg = vars_renderer.render_str(arg.clone())?;
+                let rendered_arg = vars_renderer.render_str(&arg)?;
                 rendered_args.push(rendered_arg);
             }
             rendered_program_config.args = Some(rendered_args);
@@ -131,16 +133,16 @@ impl RenderedProgramConfig {
         program_name: &str,
         vars_renderer: &mut VarsRenderer,
     ) -> Result<LoggerConfig, tera::Error> {
-        vars_renderer.add_ctx_vars(HashMap::from([(
+        vars_renderer.add_ctx_vars(&HashMap::from([(
             "program".to_string(),
             tera::to_value(program_name).unwrap(),
         )]));
         let mut rendered_logger_config = logger_config.clone();
-        rendered_logger_config.command = vars_renderer.render_str(logger_config.command.clone())?;
+        rendered_logger_config.command = vars_renderer.render_str(&logger_config.command)?;
         if let Some(args) = &logger_config.args {
             let mut rendered_args: Vec<String> = vec![];
             for arg in args {
-                let rendered_arg = vars_renderer.render_str(arg.clone())?;
+                let rendered_arg = vars_renderer.render_str(&arg)?;
                 rendered_args.push(rendered_arg);
             }
             rendered_logger_config.args = Some(rendered_args);
@@ -153,24 +155,62 @@ impl RenderedProgramConfig {
 struct VarsRenderer {
     tera: tera::Tera,
     tera_ctx: tera::Context,
+    chayd_obj: HashMap<String, tera::Value>,
+    env_obj: HashMap<String, String>,
 }
 
 impl VarsRenderer {
-    pub fn new(vars_config: &crate::config::VarsConfig) -> Self {
+    pub fn new(vars_config: &crate::config::VarsConfig) -> tera::Result<Self> {
         let mut vars_renderer = Self::default();
-        vars_renderer.tera = tera::Tera::default();
-        for (vars_table_name, vars_table) in &mut vars_config.iter() {
-            vars_renderer.tera_ctx.insert(vars_table_name, &vars_table);
+        vars_renderer.add_system_env_vars();
+        let rendered_vars_config = vars_renderer.render_vars_config(vars_config)?;
+        for (vars_table_name, vars_hashmap) in rendered_vars_config {
+            vars_renderer
+                .tera_ctx
+                .insert(vars_table_name, &vars_hashmap);
         }
-        vars_renderer
+        Ok(vars_renderer)
     }
 
-    pub fn add_ctx_vars(&mut self, ctx_vars: HashMap<String, tera::Value>) {
-        self.tera_ctx
-            .insert("chayd", &HashMap::from([("ctx", ctx_vars)]));
+    pub fn add_ctx_vars(&mut self, ctx_vars: &HashMap<String, tera::Value>) {
+        self.chayd_obj
+            .insert("ctx".to_string(), tera::to_value(ctx_vars).unwrap());
+        // Over-write the previous chayd object in the tera_ctx. Ths is why we have to keep a
+        // separate HashMap for chayd_obj. There is no way (that I could find) to modify an
+        // existing object in the tera_ctx.
+        self.tera_ctx.insert("chayd", &self.chayd_obj);
     }
 
-    pub fn render_str(&mut self, config_str: String) -> tera::Result<String> {
+    pub fn add_env_vars(&mut self, env_vars: &HashMap<String, String>) {
+        self.env_obj.extend(env_vars.clone());
+        // Over-write the previous env object in the tera_ctx. Ths is why we have to keep a
+        // separate HashMap for env_obj. There is no way (that I could find) to modify an
+        // existing object in the tera_ctx.
+        self.tera_ctx.insert("env", &self.env_obj);
+    }
+
+    pub fn add_system_env_vars(&mut self) {
+        let mut env_vars = HashMap::new();
+        std::env::vars().for_each(|(env_var, value)| {
+            env_vars.insert(env_var, value);
+        });
+        self.add_env_vars(&env_vars);
+    }
+
+    pub fn render_vars_config(
+        &mut self,
+        vars_config: &crate::config::VarsConfig,
+    ) -> tera::Result<crate::config::VarsConfig> {
+        let mut rendered_vars_config = vars_config.clone();
+        for vars_hashmap in rendered_vars_config.values_mut() {
+            for value in vars_hashmap.values_mut() {
+                *value = self.render_str(&value)?;
+            }
+        }
+        Ok(rendered_vars_config)
+    }
+
+    pub fn render_str(&mut self, config_str: &str) -> tera::Result<String> {
         self.tera.render_str(&config_str, &self.tera_ctx)
     }
 }
